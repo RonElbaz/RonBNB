53444106146ef08414ee58cb59706ad7ce9d027b
Amit <amit9532918@gmail.com>
2022-07-20 14:55:33 +0300

    stay-list

--- frontend/src/components/stay-preview.vue
+++ frontend/src/components/stay-preview.vue
@@ -1,15 +1,15 @@
 <template>
-    <section class="stay-preview">
-        <div class="block text-center" m="t-4">
+    <section @click="moveToDetails" class="stay-preview">
+        <!-- <div class="block text-center" m="t-4">
             <el-carousel trigger="click" height="150px">
-                <el-carousel-item v-for="item in stay.imgUrls" :key="item">
-                    <h3 class="small justify-center" text="2xl">../images/{{ item }}</h3>
-                    <img class="small justify-center" :src="`../images/${item}`">
-                    <!-- <img class="small justify-center" src="../images/045.jpeg"> -->
+                <el-carousel-item v-for="image in stay.imgUrls" :key="image">
+                    <h3 class="small justify-center" text="2xl">../images/{{ image }}</h3>
+                    <img class="small justify-center" :src="'../images/stay-images/' + image" />
+                    <img class="small justify-center" src="../images/stay-images/045.jpeg">
                 </el-carousel-item>
             </el-carousel>
         </div>
-        <h1>{{ stay.name }}</h1>
+        <h1>{{ stay.address.city }} ,<span>{{ stay.address.country }}</span></h1> -->
         <h1>${{ stay.price }} night</h1>
     </section>
 </template>
@@ -28,6 +28,9 @@ export default {
     components: {
     },
     methods: {
+        moveToDetails() {
+            this.$router.push(`/stay/details/${this.stay._id}`)
+        },
     },
     computed: {
     }
--- frontend/src/services/stay-service.js
+++ frontend/src/services/stay-service.js
@@ -3,12 +3,13 @@ import { storageService } from "./storage-service.js"
 export const stayService = {
     query,
     getById,
+    getRandomInt
 }
 
 const DB_KEY = "stayDB"
 _createStays()
 
-async function query(filterBy = null){
+async function query(filterBy = null) {
     // if(!filterBy) return {key:"onsucS"}
 
     // var stays = await (await fetch('../../data/stay.json')).json()
@@ -18,16 +19,29 @@ async function query(filterBy = null){
     return storageService.query(DB_KEY)
 }
 
-async function getById(stayId){
-    return storageService.getById(stayId)
+async function getById(stayId) {
+    try {
+        var stay = await storageService.get(DB_KEY, stayId)
+        return stay
+
+    } catch (error) {
+        console.log(error)
+    }
+
 }
 
-async function _createStays(){
+async function _createStays() {
     var stays = JSON.parse(localStorage.getItem(DB_KEY))
-    if(!stays || !stays.length){
-        stays =  await (await fetch('../../data/stay.json')).json()
-        stays = stays.splice(0,10)
+    if (!stays || !stays.length) {
+        stays = await (await fetch('../../data/stay.json')).json()
+        stays = stays.splice(0, 10)
         console.log(stays)
-        localStorage.setItem(DB_KEY,JSON.stringify(stays))
+        localStorage.setItem(DB_KEY, JSON.stringify(stays))
     }
 }
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min 
+  }
