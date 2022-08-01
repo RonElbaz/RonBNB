7981617d2af644810722e96a29a878c6a4b83d6f
RonElbaz <RonElbaz@users.noreply.github.com>
2022-08-01 12:35:20 +0300

    push to pull

--- frontend/src/services/order-service.js
+++ frontend/src/services/order-service.js
@@ -80,74 +80,74 @@ async function addOrder(order){
   //   }
   // }
 
-  function _createOrder(){
-    var orders =  [
-        {
-          "_id": "o1225",
-          "hostId": "u102",
-          "createdAt": 9898989,
-          "buyer": {
-            "_id": "u101",
-            "fullname": "User 1"
-          },
-          "totalPrice": 160,
-          "startDate": "2025/10/15",
-          "endDate": "2025/10/17",
-          "guests": {
-            "adults": 2,
-            "kids": 1
-          },
-          "stay": {
-            "_id": "h102",
-            "name": "House Of Uncle My",
-            "price": 80
-          },
-          "status": "Pending"
-        },    
-        {
-          "_id": "o1337",
-          "hostId": "u103",
-          "createdAt": 9898989,
-          "buyer": {
-            "_id": "u105",
-            "fullname": "User 3"
-          },
-          "totalPrice": 100,
-          "startDate": "2025/10/15",
-          "endDate": "2025/10/17",
-          "guests": {
-            "adults": 2,
-            "kids": 4
-          },
-          "stay": {
-            "_id": "fssd233",
-            "name": "House Of king obama",
-            "price": 20
-          },
-          "status": "Pending"
-        },    
-        {
-          "_id": "oShit121",
-          "hostId": "u120",
-          "createdAt": 9898989,
-          "buyer": {
-            "_id": "u101",
-            "fullname": "User 5"
-          },
-          "totalPrice": 300,
-          "startDate": "2025/10/15",
-          "endDate": "2025/10/17",
-          "guests": {
-            "adults": 2,
-            "kids": 1
-          },
-          "stay": {
-            "_id": "h200",
-            "name": "House Of fun",
-            "price": 100
-          },
-          "status": "Pending"
-        },    
-      ]
-      return orders
-  }
+  // function _createOrder(){
+  //   var orders =  [
+  //       {
+  //         "_id": "o1225",
+  //         "hostId": "u102",
+  //         "createdAt": 9898989,
+  //         "buyer": {
+  //           "_id": "u101",
+  //           "fullname": "User 1"
+  //         },
+  //         "totalPrice": 160,
+  //         "startDate": "2025/10/15",
+  //         "endDate": "2025/10/17",
+  //         "guests": {
+  //           "adults": 2,
+  //           "kids": 1
+  //         },
+  //         "stay": {
+  //           "_id": "h102",
+  //           "name": "House Of Uncle My",
+  //           "price": 80
+  //         },
+  //         "status": "Pending"
+  //       },    
+  //       {
+  //         "_id": "o1337",
+  //         "hostId": "u103",
+  //         "createdAt": 9898989,
+  //         "buyer": {
+  //           "_id": "u105",
+  //           "fullname": "User 3"
+  //         },
+  //         "totalPrice": 100,
+  //         "startDate": "2025/10/15",
+  //         "endDate": "2025/10/17",
+  //         "guests": {
+  //           "adults": 2,
+  //           "kids": 4
+  //         },
+  //         "stay": {
+  //           "_id": "fssd233",
+  //           "name": "House Of king obama",
+  //           "price": 20
+  //         },
+  //         "status": "Pending"
+  //       },    
+  //       {
+  //         "_id": "oShit121",
+  //         "hostId": "u120",
+  //         "createdAt": 9898989,
+  //         "buyer": {
+  //           "_id": "u101",
+  //           "fullname": "User 5"
+  //         },
+  //         "totalPrice": 300,
+  //         "startDate": "2025/10/15",
+  //         "endDate": "2025/10/17",
+  //         "guests": {
+  //           "adults": 2,
+  //           "kids": 1
+  //         },
+  //         "stay": {
+  //           "_id": "h200",
+  //           "name": "House Of fun",
+  //           "price": 100
+  //         },
+  //         "status": "Pending"
+  //       },    
+  //     ]
+  //     return orders
+  // }
